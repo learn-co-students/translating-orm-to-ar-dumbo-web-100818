2d321ee2872e37938fd8927e1b5a8aac3cb47a32
github username <github email address>
2018-10-22 17:30:17 -0400

    finished

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ActiveRecord::Base
 end
+
+
+# -git add . #add the changes you've made in the file tree
+# -git status #lets you see the files that are staged to be commited
+# -git commit -m ""   #this is adding a message on what you've udpated
+# -git push           #push to git
